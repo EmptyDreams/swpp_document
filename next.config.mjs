fd87377d5e4e78e1473d76f7ec9c50968459ba5b
Redish101 <i@redish101.top>
2024-08-14 09:09:44 +0800

    chore: 代码格式化

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
   output: "export", // if want use ssr, remove this.
   images: {
     unoptimized: true, // image optimization cannot work with ssg
-  }
-}
+  },
+};
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
