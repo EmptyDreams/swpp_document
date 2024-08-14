643c6d1d18de9464417e25fd4228bd4c68c39388
Redish101 <i@redish101.top>
2024-08-14 18:33:09 +0800

    chore: 补全`tsconfig`和`prettierrc`

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
-import nextra from "nextra";
+import nextra from 'nextra';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // if want use ssr, remove this.
+  output: 'export', // if want use ssr, remove this.
   images: {
     unoptimized: true, // image optimization cannot work with ssg
   },
 };
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
 });
 
 export default withNextra(nextConfig);
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import nextraConfig from "redish101-shared-config/nextra";
+import React from 'react';
+import nextraConfig from 'redish101-shared-config/nextra';
 
-const siteName = "swpp";
+const siteName = 'swpp';
 
 export default {
   logo: <span>{siteName}</span>,
   project: {
-    link: "https://github.com/EmptyDreams/swpp-backends",
+    link: 'https://github.com/EmptyDreams/swpp-backends',
   },
-  docsRepositoryBase: "https://github.com/EmptyDreams/swpp_document/tree/main/",
+  docsRepositoryBase: 'https://github.com/EmptyDreams/swpp_document/tree/main/',
   useNextSeoProps() {
     return {
       titleTemplate: `%s – ${siteName}`,
